1474c418c52964eb5fb2155d2a86a6c9f46cbdd9
wuyande <wuyd25@chinaunicom.cn>
2018-01-03 09:32:54 +0800

    Initial commit

---
+++ src/routes/Redis/Workplace.js
@@ -0,0 +1,174 @@
+import React, { PureComponent } from 'react';
+import moment from 'moment';
+import { connect } from 'dva';
+import { Link } from 'dva/router';
+import { Row, Col, Card, List, Avatar } from 'antd';
+
+import PageHeaderLayout from '../../layouts/PageHeaderLayout';
+import EditableLinkGroup from '../../components/EditableLinkGroup';
+import { Radar } from '../../components/Charts';
+
+import styles from './Workplace.less';
+
+const links = [
+  {
+    title: '操作一',
+    href: '',
+  },
+  {
+    title: '操作二',
+    href: '',
+  },
+  {
+    title: '操作三',
+    href: '',
+  },
+  {
+    title: '操作四',
+    href: '',
+  },
+  {
+    title: '操作五',
+    href: '',
+  },
+  {
+    title: '操作六',
+    href: '',
+  },
+];
+
+const members = [
+  {
+    id: 'members-1',
+    title: '科学搬砖组',
+    logo: 'https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png',
+    link: '',
+  },
+  {
+    id: 'members-2',
+    title: '程序员日常',
+    logo: 'https://gw.alipayobjects.com/zos/rmsportal/cnrhVkzwxjPwAaCfPbdc.png',
+    link: '',
+  },
+  {
+    id: 'members-3',
+    title: '设计天团',
+    logo: 'https://gw.alipayobjects.com/zos/rmsportal/gaOngJwsRYRaVAuXXcmB.png',
+    link: '',
+  },
+  {
+    id: 'members-4',
+    title: '中二少女团',
+    logo: 'https://gw.alipayobjects.com/zos/rmsportal/ubnKSIfAJTxIgXOKlciN.png',
+    link: '',
+  },
+  {
+    id: 'members-5',
+    title: '骗你学计算机',
+    logo: 'https://gw.alipayobjects.com/zos/rmsportal/WhxKECPNujWoWEFNdnJE.png',
+    link: '',
+  },
+];
+
+@connect(state => ({
+  project: state.project,
+  activities: state.activities,
+  chart: state.chart,
+}))
+export default class Workplace extends PureComponent {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'project/fetchNotice',
+    });
+    dispatch({
+      type: 'activities/fetchList',
+    });
+    dispatch({
+      type: 'chart/fetch',
+    });
+  }
+
+  componentWillUnmount() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'chart/clear',
+    });
+  }
+
+  renderActivities() {
+    const {
+      activities: { list },
+    } = this.props;
+    return list.map((item) => {
+      const events = item.template.split(/@\{([^{}]*)\}/gi).map((key) => {
+        if (item[key]) {
+          return <a href={item[key].link} key={item[key].name}>{item[key].name}</a>;
+        }
+        return key;
+      });
+      return (
+        <List.Item key={item.id}>
+          <List.Item.Meta
+            avatar={<Avatar src={item.user.avatar} />}
+            title={
+              <span>
+                <a className={styles.username}>{item.user.name}</a>
+                &nbsp;
+                <span className={styles.event}>{events}</span>
+              </span>
+            }
+            description={
+              <span className={styles.datetime} title={item.updatedAt}>
+                {moment(item.updatedAt).fromNow()}
+              </span>
+            }
+          />
+        </List.Item>
+      );
+    });
+  }
+
+  render() {
+    const {
+      project: { loading: projectLoading, notice },
+      activities: { loading: activitiesLoading },
+      chart: { radarData },
+    } = this.props;
+
+    const pageHeaderContent = (
+      <div className={styles.pageHeaderContent}>
+        <div className={styles.avatar}>
+          <Avatar size="large" src="https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png" />
+        </div>
+        <div className={styles.content}>
+          <div className={styles.contentTitle}>早安，曲丽丽，祝你开心每一天！</div>
+          <div>交互专家 | 蚂蚁金服－某某某事业群－某某平台部－某某技术部－UED</div>
+        </div>
+      </div>
+    );
+
+    const pageHeaderExtra = (
+      <div className={styles.pageHeaderExtra}>
+        <div>
+          <p>项目数</p>
+          <p>56</p>
+        </div>
+        <div>
+          <p>团队内排名</p>
+          <p>8<span> / 24</span></p>
+        </div>
+        <div>
+          <p>项目访问</p>
+          <p>2,223</p>
+        </div>
+      </div>
+    );
+
+    return (
+        <div>
+            <iframe title="myiframe" src="http://www.baidu.com" width="600px" height="400px"></iframe>
+        </div>
+    );
+  }
+}
